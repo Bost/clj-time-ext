6d388f7a571bf66bba64691e07cde899cea7deb8
Bost <thebost@gmail.com>
2015-07-22 19:42:02 +0200

    org.clojure/clojure "1.7.0" causes problems

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [
+                 ;; [org.clojure/clojure "1.7.0"] ; causes problems
+                 [org.clojure/clojure "1.6.0"]
                  [commons-io/commons-io "2.4"]
-                 [clj-time "0.9.0"]
+                 [clj-time "0.10.0"]
                  ]
   :main clj-time-ext.core
   :profiles {:uberjar {:aot :all}})
