b1a91e26f6f4b5a0db2d6f1b8e4f81c49256bd91
Bost <thebost@gmail.com>
2015-07-23 15:47:20 +0200

    modified-diff, even shorter non-verbose str, bugfixes
    bugfixes for ' ' when e.g. .getHour returns 0

--- src/clj_time_ext/core.clj
+++ src/clj_time_ext/core.clj
@@ -1,7 +1,7 @@
 (ns clj-time-ext.core
   (:require [clj-time.core :as t]
             [clj-time.coerce :as tc])
-  (import org.apache.commons.io.FilenameUtils
+  (:import org.apache.commons.io.FilenameUtils
           org.joda.time.format.PeriodFormatterBuilder
           org.joda.time.DateTime)
   (:gen-class))
@@ -10,30 +10,14 @@
   (let [m (.lastModified (new java.io.File filepath))]
     (tc/from-long m)))
 
-(defn modified-ago
-  "Usage: (modified-ago datetime-tstp :verbose true)"
-  [datetime-tstp & {:keys [verbose] :or {verbose false}}]
-  (let [period (-> (t/interval datetime-tstp (t/now))
+(defn modified-diff
+  "Typical usage: (modified-diff datetime-tstp (t/now) :verbose true)"
+  [datetime-tstp datetime-tstp-now & {:keys [verbose] :or {verbose false}}]
+  (let [period (-> (t/interval datetime-tstp
+                               datetime-tstp-now)
                    .toPeriod)
         formatter
         (if verbose
-          (-> (new PeriodFormatterBuilder)
-              .appendYears
-              (.appendSuffix " year, " " years, ")
-              .appendMonths
-              (.appendSuffix " month, " " months, ")
-              .appendWeeks
-              (.appendSuffix " week, " " weeks, ")
-              .appendDays
-              (.appendSuffix " day, " " days, ")
-              .appendHours
-              (.appendSuffix " hour, " " hours, ")
-              .appendMinutes
-              (.appendSuffix " minute, " " minutes, ")
-              .appendSeconds
-              (.appendSuffix " second" " seconds")
-              .printZeroNever
-              .toFormatter)
           (-> (new PeriodFormatterBuilder)
               .appendYears
               (.appendSuffix " year " " years ")
@@ -46,11 +30,48 @@
               .appendHours
               (.appendSuffix " hour " " hours ")
               .appendMinutes
-              (.appendSuffix " min" " mins")
+              (.appendSuffix " minute " " minutes ")
+              .appendSeconds
+              (.appendSuffix " second " " seconds ")
               .printZeroNever
-              .toFormatter))
+              .toFormatter)
+          (if (> (.getHours period) 0 )
+            (-> (new PeriodFormatterBuilder)
+                .appendYears
+                (.appendSuffix " year " " years ")
+                .appendMonths
+                (.appendSuffix " month " " months ")
+                .appendWeeks
+                (.appendSuffix " week " " weeks ")
+                .appendDays
+                (.appendSuffix " day " " days ")
+                .appendHours
+                (.appendSuffix " hour " " hours ")
+                .printZeroNever
+                .toFormatter)
+            (-> (new PeriodFormatterBuilder)
+                .appendYears
+                (.appendSuffix " year " " years ")
+                .appendMonths
+                (.appendSuffix " month " " months ")
+                .appendWeeks
+                (.appendSuffix " week " " weeks ")
+                .appendDays
+                (.appendSuffix " day " " days ")
+                .appendHours
+                (.appendSuffix " hour " " hours ")
+                .appendMinutes
+                (.appendSuffix " min " " mins ")
+                .printZeroNever
+                .toFormatter)))
         ]
-    (str (.print formatter period) (if verbose " ago" ""))))
+    (str (clojure.string/trimr (.print formatter period))
+         (if verbose " ago" ""))))
+
+(defn modified-ago
+  "Usage: (modified-ago datetime-tstp :verbose true)"
+  [datetime-tstp & {:keys [verbose] :or {verbose false}}]
+  (modified-diff datetime-tstp (t/now) :verbose verbose))
 
 (defn file-modified-ago
   "Usage: (file-modified-ago filepath :verbose true)"
