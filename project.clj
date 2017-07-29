88160dc40377afdba6d3c77c9f179938d562c96f
Bost <thebost@gmail.com>
2017-07-29 14:28:27 +0200

    Upgrade deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [commons-io/commons-io "2.5"]
-                 [clj-time "0.13.0"]]
+                 [clj-time "0.14.0"]]
   :main clj-time-ext.core
   :profiles {:uberjar {:aot :all}})
