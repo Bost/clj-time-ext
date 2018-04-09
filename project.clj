96ae998974747632b6a1fb9fc592ebd8498cfcb2
Bost <thebost@gmail.com>
2018-04-09 15:24:42 +0200

    Upgrade deps, bump to 0.15.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-time-ext "0.14.0"
+(defproject clj-time-ext "0.15.0"
   :description "Extention of clj-time"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [commons-io/commons-io "2.6"]
-                 [clj-time "0.14.2"]]
+                 [clj-time "0.14.3"]]
   :main clj-time-ext.core
   :profiles {:uberjar {:aot :all}})
