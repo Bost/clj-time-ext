bb3fb9b4efed15ca27282fd0d367b5671657af4d
Bost <thebost@gmail.com>
2018-05-16 14:52:49 +0200

    Upgrade clj-time dep

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-time-ext "0.15.0"
+(defproject clj-time-ext "0.15.1"
   :description "Extention of clj-time"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [commons-io/commons-io "2.6"]
-                 [clj-time "0.14.3"]]
+                 [clj-time "0.14.4"]]
   :main clj-time-ext.core
   :profiles {:uberjar {:aot :all}})
