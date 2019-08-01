3c3378e81b74a8c9e466ac93a13b7d77e46e8ede
Bost <thebost@gmail.com>
2019-08-01 17:29:07 +0200

    Upgrade deps; clojar deployment

--- project.clj
+++ project.clj
@@ -6,10 +6,14 @@ https://mvnrepository.com/artifact/org.ocpsoft.prettytime/prettytime"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-time "0.15.1"]
                  [com.darwinsys/hirondelle-date4j "1.5.1"]]
   ;; can't use the lein-v from ~/.lein/profiles.clj
-  :plugins [[com.roomkey/lein-v "7.0.0"]]
+  :plugins [[com.roomkey/lein-v "7.1.0"]]
   :main clj-time-ext.core
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  ;; enter user/password on the command line
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :sign-releases false}]]
+  )
