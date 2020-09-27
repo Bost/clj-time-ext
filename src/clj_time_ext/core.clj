3d91948e4f65420771876aee5f0e76cf7857434b
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2020-09-27 22:26:30 +0200

    Timestamp fn can be specified with respect to a timezone.

--- src/clj_time_ext/core.clj
+++ src/clj_time_ext/core.clj
@@ -100,26 +100,75 @@
   (ago-diff (new DateTime tstp) prm-map))
 
 (defn tstp
-  "Return a string containing current timestamp, formatted using `pattern` and
-  shortened to the length of the `pattern`. E.g.:
+  "Return a string with current timestamp, formatted using `pattern` and shortened
+  to the length of the `pattern` for a given timezone. E.g.:
   (tstp \"HHmmss.nnn\")
+  ;; => \"161644.809\"
+  (tstp \"HHmmss.nnn\" \"Europe/London\")
+  ;; => \"151644.809\"
+  (tstp \"HHmmss.nnn\" \"Europe/Berlin\")
+  ;; => \"161644.809\"
+  "
+  ([] (tstp "HHmmss.nnn" (str (java.time.ZoneId/systemDefault))))
+  ([pattern] (tstp pattern (str (java.time.ZoneId/systemDefault))))
+  ([pattern zone-id]
+   (let [
+         ;; pattern "HHmmss.nnnn"
+         fmt (java.time.format.DateTimeFormatter/ofPattern pattern)
+         date (java.time.LocalDateTime/now (java.time.ZoneId/of zone-id))]
+     (subs (.format date fmt) 0 (count pattern)))))
+
+(defn tstp3
+  "Return a string with current 3-digits timestamp. E.g.:
+  (tstp)
   ;; => 112147.123"
-  [pattern]
-  (let [
-        ;; pattern "HHmmss.nnnn"
-        fmt (java.time.format.DateTimeFormatter/ofPattern pattern)
-        date (java.time.LocalDateTime/now)]
-    (subs (.format date fmt) 0 (count pattern))))
+  [] (tstp))
 
-(defn tstp3 "timestamp 112147.123"       [] (tstp "HHmmss.nnn"))
-(defn tstp4 "timestamp 112147.1234"      [] (tstp "HHmmss.nnnn"))
-(defn tstp5 "timestamp 112147.12345"     [] (tstp "HHmmss.nnnnn"))
-(defn tstp6 "timestamp 112147.123456"    [] (tstp "HHmmss.nnnnnn"))
-(defn tstp7 "timestamp 112147.1234567"   [] (tstp "HHmmss.nnnnnnn"))
-(defn tstp8 "timestamp 112147.12345678"  [] (tstp "HHmmss.nnnnnnnn"))
-(defn tstp9 "timestamp 112147.123456789" [] (tstp "HHmmss.nnnnnnnnn"))
+(defn tstp4
+  "Return a string with current 4-digits timestamp. E.g.:
+  (tstp \"HHmmss.nnnn\")
+  ;; => 112147.1234"
+  [] (tstp "HHmmss.nnnn"))
 
-(defn tnow "timestamp 112147.123"        [] (tstp3))
+(defn tstp5
+  "Return a string with current 5-digits timestamp. E.g.:
+  (tstp \"HHmmss.nnnnn\")
+  ;; => 112147.12345"
+  [] (tstp "HHmmss.nnnnn"))
+
+(defn tstp6
+  "Return a string with current 6-digits timestamp. E.g.:
+  (tstp \"HHmmss.nnnnnn\")
+  ;; => 112147.123456"
+  [] (tstp "HHmmss.nnnnnn"))
+
+(defn tstp7
+  "Return a string with current 7-digits timestamp. E.g.:
+  (tstp \"HHmmss.nnnnnnn\")
+  ;; => 112147.1234567"
+  [] (tstp "HHmmss.nnnnnnn"))
+
+(defn tstp8
+  "Return a string with current 8-digits timestamp. E.g.:
+  (tstp \"HHmmss.nnnnnnnn\")
+  ;; => 112147.12345678"
+  [] (tstp "HHmmss.nnnnnnnn"))
+
+(defn tstp9
+  "Return a string with current 9-digits timestamp. E.g.:
+  (tstp \"HHmmss.nnnnnnnnn\")
+  ;; => 112147.123456789"
+  [] (tstp "HHmmss.nnnnnnnnn"))
+
+(defn tnow
+  "Return a string containing current timestamp. E.g.
+  (tnow)
+  ;; => \"161644.809\"
+  (tnow \"Europe/London)
+  ;; => \"151644.809\"
+  "
+  ([] (tstp3))
+  ([zone-id] (tstp "HHmmss.nnn" zone-id)))
 
 (defn fntime [v]
   (tf/unparse (tf/formatter "HH:mm dd.MM.yy")
